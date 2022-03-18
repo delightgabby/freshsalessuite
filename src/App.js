5a2d3e636b170784451956126fb091d0192409d0
delightgabby <USER@DESKTOP-VTQCVES>
2022-03-18 19:19:33 +0100

    uncensored

--- src/App.js
+++ src/App.js
@@ -1,25 +1,356 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./FRESHSALES IMAGES/39.png";
+import logo2 from "./FRESHSALES IMAGES/38.svg";
+import pic from "./FRESHSALES IMAGES/h1.png";
+import b1pic1 from "./FRESHSALES IMAGES/1.png";
+import b1pic2 from "./FRESHSALES IMAGES/2.png";
+import b1pic3 from "./FRESHSALES IMAGES/5.png";
+import b1pic4 from "./FRESHSALES IMAGES/4.png";
+import svg1 from "./FRESHSALES IMAGES/9.svg";
+import svg2 from "./FRESHSALES IMAGES/7.svg";
+import svg3 from "./FRESHSALES IMAGES/8.svg";
+import pic1 from "./FRESHSALES IMAGES/10.png";
+import icon1 from "./FRESHSALES IMAGES/19.svg";
+import icon2 from "./FRESHSALES IMAGES/17.svg";
+import icon3 from "./FRESHSALES IMAGES/15.svg";
+import icon4 from "./FRESHSALES IMAGES/16.svg";
+import pic2 from "./FRESHSALES IMAGES/11.png";
+import icon5 from "./FRESHSALES IMAGES/20.svg";
+import icon6 from "./FRESHSALES IMAGES/30.svg";
+import icon7 from "./FRESHSALES IMAGES/27.svg";
+import icon8 from "./FRESHSALES IMAGES/31.svg";
+import pic3 from "./FRESHSALES IMAGES/12.png";
+import icon9 from "./FRESHSALES IMAGES/32.svg";
+import icon10 from "./FRESHSALES IMAGES/35.svg";
+import icon11 from "./FRESHSALES IMAGES/36.svg";
+import pic4 from "./FRESHSALES IMAGES/13.png";
+import icon12 from "./FRESHSALES IMAGES/26.svg";
+import icon13 from "./FRESHSALES IMAGES/22.svg";
+import icon14 from "./FRESHSALES IMAGES/32.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+
+
+    
+    return(
+     
+      <div>
+      <div class="tophead1">
+      <div class="thd1"> 
+      <div class="thda">
+        <img class="lg"src={logo} alt="logo"/>
+      </div>
+      <div class="thdb">
+      <div>Features</div>
+        <div>Pricing</div>
+        <div>Resources</div>
+        <div>Industries</div>
+        <div>Login</div>
+      </div>
+      <button class="hdc">SIGN UP</button>
+      </div>
+      </div>
+  {/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+  <div class="tophead">
+      <div class="hd1"> 
+      <div class="hda">
+      <img class="lg2"src={logo2} alt="logo"/>
+      </div>
+      <div class="hdb">
+        <div>Products</div>
+        <div>Platform</div>
+        <div>Company</div>
+        <div>Customers</div>
+        <div>Support</div>
+      </div>
+        </div>
+      </div>
+{/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+  <div class="tophead2">
+      <div class="thd1"> 
+      <div class="thda">Freshsales Suite</div>
+      <div class="thdb">
+      <div>Features</div>
+        <div>Pricing</div>
+        <div>Resources</div>
+        <div>Industries</div>
+        <div>Login</div>
+      </div>
+      <button class="hdc">SIGN UP</button>
+      </div>
+      </div>
+
+
+
+{/* ++++++++++++++++++++++++++++++++++++++++ */}
+        <div class="mherocon">
+          
+            <div class="herocon">
+           <div class="box1">
+              <h1>
+              <div class="crm">
+                  <span>  CRM</span>
+                </div>
+            Unite sales and marketing
+              around the customer with
+              an all-in-one CRM</h1>
+        
+              <h2>
+              know customer intent to help sales convert  faster with a 360&#176; view
+              </h2>
+                <div class="btn">
+              <button class="btn1">SIGN UP</button>
+              <button class="btn2">VIEW PRICING</button>
+              </div>
+
+           </div>
+           <div class="box2">
+           <img src={pic} alt="pic"/>
+           </div>
+        </div>
+        </div>
+
+{/* +++++++++++++++++++++++++++++++++++++++++++++++++++++= */}
+
+<div class="word">
+<div class="iword">
+            Analysts and end-users love Freshsales
+        </div>
+</div>
+
+<div class="mherocon2">
+          
+            <div class="herocon2">
+           <div class="b1">
+           <div><img class="p1" src={b1pic1} alt="b1pic"/></div>
+           <div class="b">Gartner Magic
+           Quadrant 2021 - Sales
+           Force Automation</div>
+           </div>
+           <div class="b1">
+             <img class="p1" src={b1pic2} alt="b1pic"/>
+             <div class="b">Gartner Magic
+           Quadrant 2020 - CRM
+           lead Management</div>
+           </div>
+           <div class="b1"><img class="p1x" src={b1pic3} alt="b1pic"/>
+           <div class="b">G2 CRM Software Leader 2021</div>
+           </div>
+           <div class="b1"><img class="p1" src={b1pic4} alt="b1pic"/>
+           <div class="b">GetApp CRM Software Leaders 2019.</div>
+           </div>
+        </div>
+
+        </div>
+{/* +++++++++++++++++++++++++++++++++++++++++++++++++++++= */}
+
+<div class="word1">
+          How can Freshsales Suite help you grow your business?
+</div>
+
+<div class="mherocon3">
+          
+            <div class="herocon3">
+           <div class="b2">
+           <div><img class="p1" src={svg1} alt="b1pic"/></div>
+           <div class="c"><b>Nurture relationships and grow revenue with a single customer view</b><br />
+Deliver seamless engagement across channels with a 360° view of the customer. Unify sales and marketing teams to deliver customer delight.
+</div>
+           </div>
+           <div class="b2">
+             <img class="p1" src={svg2} alt="b1pic"/>
+             <div class="c"><b>Close deals faster by sending the right message at the right time</b><br />
+Sales and marketing can work together to deliver personalized customer experiences. 
+Understand customer intent and send the right message to close deals faster.</div>
+           </div>
+           <div class="b2"><img class="p1" src={svg3} alt="b1pic"/>
+           <div class="c">
+           <b>Reduce IT complexity & cost with a comprehensive solution</b><br />
+Keep teams happy with a single, all-in-one solution that is affordable, easy to implement, and easier to use.
+ Increase adoption, reduce costs and boost morale.
+           </div>
+           </div>
+           
+        </div>
+        </div>
+
+{/* ++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+
+
+<div class="mherocon4">
+          
+            <div class="herocon4">
+
+           <div class="b2a">
+           <img class="p1a" src={pic1} alt="b1pic"/>
+           </div>
+
+           <div class="b2a">
+             <div class="ca">
+               <div class="find">FIND MORE OPPORTUNITIES</div><br />
+               <div class="Attract">Attract the best leads. Generate 10X more opportunities.</div><br />
+               <div class="Attract2">Attract visitors, convert them into leads, run campaigns, and turn your website into a lead generation machine.</div><br />
+               <div class="top">Top features:</div><br />
+               <div class="create">
+               <img class="ic"src={icon1} alt="logo"/>&#160;&#160;&#160;&#160;
+                 Create <b>landing pages</b> in minutes. Monitor and understand &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;visitor intent.</div><br />
+               <div class="understand">
+               <img class="ic"src={icon2} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Understand visitor intent. Track <b>page visits and even actions </b> &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;on your site.</div><br />
+               <div class="chat">
+               <img class="ic"src={icon3} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Chat in real-time with website visitors using <b>AI-powered &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;chatbots</b></div><br />
+               <div class="tailor">
+               <img class="ic"src={icon4} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Tailor <b>email campaigns</b> and generate cost-effective leads for &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;sales</div><br />
+                 <br />
+               <div class="learn">
+              Learn more &#8594;
+              </div>
+            </div>
+           </div>
+           
+        </div>
+        </div>
+{/* +++++++++++++++++++++++++++++++++++++++++++++ */}
+
+<div class="mherocon5">
+          
+            <div class="herocon4">
+           <div class="b2a">
+           <div class="ca">
+               <div class="findz">ENGAGE CONTEXTUALLY</div><br />
+               <div class="Attract">Deliver personalized experiences at scale. Improve lead engagement.
+             </div><br />
+               <div class="Attract2">Build tailored campaigns and optimize them with real-time data for better engagement.</div><br />
+               <div class="top">Top features:</div><br />
+               <div class="create">
+               <img class="ix"src={icon5} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Get a <b>360° view of a customer.</b> Capture all customer 
+                 &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;interactions in one place.</div><br />
+               <div class="understand">
+               <img class="ic"src={icon6} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+               <b> Freddy AI</b> identifies your best leads - the ones that are most &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;likely to convert</div><br />
+               <div class="chat"> 
+               <img class="ic"src={icon7} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                Set up welcome emails or <b>personalized journeys</b> based on &#160;&#160;&#160;&nbsp;&#160;&#160;&#160;&#160;&#160;&#160;actions</div><br />
+               <div class="tailor">
+               <img class="ic"src={icon8} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                Communicate across <b>Phone, Email, WhatsApp, Chat</b></div><br />
+                <div class="learn">
+              Learn more &#8594;
+              </div>
+              </div>
+           <div class="c">
+            </div>
+           </div>
+           <div class="b2a">
+             <div class="c"><img class="p1a" src={pic2} alt="b1pic"/>
+             </div>
+           </div>
+           
+        </div>
+        </div>
+{/* +++++++++++++++++++++++++++++++++++++++++++++ */}
+
+<div class="mherocon4">
+          
+            <div class="herocon4">
+
+           <div class="b2a">
+           <img class="p1aa" src={pic3} alt="b1pic"/>
+           </div>
+
+           <div class="b2a">
+             <div class="ca">
+               <div class="findy">CLOSE DEALS FASTER</div><br />
+               <div class="Attract">Drive deals to closure, faster. Improve sales conversions by 50%.</div><br />
+               <div class="Attract2">Freddy AI helps you focus on the right deals that will drive revenue. 
+                Get insights across the funnel from lead to revenue with multi-touch attribution.</div><br />
+               <div class="top">Top features:</div><br />
+               <div class="create">
+               <img class="ic"src={icon9} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Improve revenue predictability with <b>Dynamic Forecasting</b></div><br />
+               <div class="understand">
+               <img class="ic"src={icon10} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Find the best deals to close with <b>AI-based Deal Insights</b></div><br />
+               <div class="chat">
+               <img class="ic"src={icon11} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+               <b> Collaborate on Slack</b> across departments to close deals faster</div><br />
+               <div class="learn">
+              Learn more &#8594;
+              </div>
+            </div>
+           </div>
+           
+        </div>
+        </div>
+{/* +++++++++++++++++++++++++++++++++++++++++++++ */}
+
+<div class="mherocon5">
+          
+            <div class="herocon4">
+           <div class="b2a">
+           <div class="ca">
+               <div class="findx">NURTURE CUSTOMER RELATIONSHIPS</div><br />
+               <div class="Attract">Develop nurture campaigns that delight customers. Increase repeat purchases by over 50%. 
+             </div><br />
+               <div class="Attract2">Nurture leads along the funnel with targeted campaigns, support customers with educational content, and build deeper relationships.</div><br />
+               <div class="top">Top features:</div><br />
+               <div class="create">
+               <img class="ic"src={icon12} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Develop <b>nurture campaigns</b> with triggers, actions, and &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;conditions </div><br />
+               <div class="understand">
+               <img class="ic"src={icon13} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+                 Send <b>transactional emails</b> to customers to upsell and &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;cross-sell</div><br />
+               <div class="chat"> 
+               <img class="ic"src={icon14} alt="logo"/>
+               &#160;&#160;&#160;&#160;
+               Get a breakdown of <b>campaign analytics</b> for a holistic &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;understanding of your market and audience</div><br />
+                <div class="learn">
+              Learn more &#8594;
+              </div>
+              </div>
+           <div class="c">
+            </div>
+           </div>
+           <div class="b2a">
+             <div class="c"><img class="p1a" src={pic4} alt="b1pic"/>
+             </div>
+           </div>
+           
+        </div>
+        </div>
+
+{/* ++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+<div class="word1">
+          Trusted by businesses worldwide
+</div>
+
+        </div>
+        
+
+
+
+    )
+ 
 }
+
 
 export default App;
